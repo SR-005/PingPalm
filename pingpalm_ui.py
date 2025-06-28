ca1be3320bcba46e3e81f15cd3a80bc32b59a11a
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-28 15:00:36 +0530

    added game background

--- pingpalm_ui.py
+++ pingpalm_ui.py
@@ -3,9 +3,14 @@ import pygame
 pygame.init()
 screen=pygame.display.set_mode((800,600)) #create the screen
 pygame.display.set_caption("Ping Palm")
+background = pygame.image.load("PingPalm_bg.jpg")  
+background = pygame.transform.scale(background, (800, 600))
+
 
 gamerun=True
 while gamerun:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             gamerun=False
+    screen.blit(background, (0, 0))
+    pygame.display.update()
