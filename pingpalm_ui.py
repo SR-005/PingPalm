aaa6619f9f1b77c32832aba40dc57c22d71350cf
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-28 15:14:06 +0530

    created paddles with set colors

--- pingpalm_ui.py
+++ pingpalm_ui.py
@@ -5,13 +5,32 @@ height=600
 width=800
 screen=pygame.display.set_mode((width,height)) #create the screen
 pygame.display.set_caption("Ping Palm")
+
+#BACKGROUND IMAGE:
 background = pygame.image.load("PingPalm_bg.jpg")  
 background = pygame.transform.scale(background, (width, height))     #scaled the background image
+
+# Game Elements
+paddlewidth, paddleheight = 10, 100
+ballx, bally = 400, 300  # screen center
+left_paddle_y = 250
+right_paddle_y = 250
+ballradius = 10
 
 gamerun=True
 while gamerun:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             gamerun=False
-    screen.blit(background, (0, 0))
-    pygame.display.update()
+
+    #draw background
+    screen.blit(background, (0, 0))  #to draw the bgimage on the screen at position (0,0)
+
+    #draw elements:
+    pygame.draw.rect(screen,(255,255,255),(50,left_paddle_y,paddlewidth,paddleheight),border_radius=10)
+    pygame.draw.rect(screen,(0,0,0),(740,right_paddle_y,paddlewidth,paddleheight),border_radius=10)
+
+
+
+
+    pygame.display.update()  #to update the frames every second
